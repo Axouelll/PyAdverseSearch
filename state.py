e8906c8f541993601da3562ffe9eac19981783ad
Benjamin <adresse.discord.exact@gmail.com>
2025-02-21 15:44:29 -0500

    added the game attribut to the State class in order to get access to the game functions

--- state.py
+++ state.py
@@ -1,18 +1,21 @@
 from abc import ABC, abstractmethod
 
 class State(ABC):
-    def __init__(self, board, player, parent=None):
+    def __init__(self, board, player, parent=None, game=None):
         """
         Generic class for all discrete-state games.
 
         :param board: Representation of the game state.
         :param player: Current player ('MAX' or 'MIN').
         :param parent: Previous state (useful for the search tree).
+        :param game: Reference to the instance of the Game class that's begin used for this state
+                        used to get access to the is_terminal function given by the user
         """
         self.board = board
         self.player = player
         self.parent = parent
         self.value = None  # Heuristic score (to be calculated later)
+        self.game = game #reference to the Game that's being played, used to get the utility function
 
     @abstractmethod
     def possible_actions(self):
@@ -24,15 +27,15 @@ class State(ABC):
         """Applies an action and returns a new state."""
         pass
 
-    @abstractmethod
+    
     def is_terminal(self):
-        """Checks if the state is a terminal state (game over)."""
-        pass
+        """Checks if the state is a terminal state (game over) thanks to the dedicated function defined by the user through the Game class"""
+        return self.game.is_terminal(self)
 
-    @abstractmethod
-    def evaluate(self):
-        """Calculates and returns the heuristic value of the state."""
-        pass
+    
+    def utility(self):
+        return self.game.utility(self)
+        
 
     def generate_successors(self):
         """Generates and returns all possible successor states."""
