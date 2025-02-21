68f2ceadd0dbc4909b0e8e062f8e9b94a51fe07a
QUEVAL Martin <116591403+MartinQueval@users.noreply.github.com>
2025-02-21 18:05:12 -0500

    correction minimax

--- class/minimax.py
+++ class/minimax.py
@@ -1,74 +1,46 @@
+import node as node
 
 """
 Implémentation de l'algorithme Minimax.
 :param Node : Le Node actuel
 :return Node: Le meilleut prochain Node
 """
-def minimax_decision(Node = None):
-    #initialise la meilleur value à - l'infini
-    best_value = -1
-    #initialise le meilleur Node à nul
-    best_Node = None
+class Minimax :
+    def __init__(self, game = None):
+        self.game = game
 
-    #On itère sur les enfants du Node
-    for n in Node.children:
-        value = min_value(n)
+    def minimax_decision(node = None):
+        #initialise la meilleur value à - l'infini
+        best_value = -1
+        #initialise le meilleur Node à nul
+        best_node = None
 
-        # si l'enfant est un meilleur cout que le précédent il
-        # devient best Node et best value
-        if value > best_value:
-            best_value = value
-            best_Node = n
+        #On itère sur les enfants du Node
+        for n in node.children:
+            value = min_value(n)
 
-    return best_Node
+            # si l'enfant est un meilleur cout que le précédent il
+            # devient best Node et best value
+            if value > best_value:
+                best_value = value
+                best_node = n
 
-def max_value(Node = None):
-    if Node.state.is_terminal():
-        return Node.state.evaluate()
+        return best_node
 
-    v = -1
-    for n in Node.children:
-        v = max(v,min_value(n))
-    return v
+    def max_value(node = None):
+        if node.state.is_terminal():
+            return node.state.evaluate()
 
-def min_value(Node = None):
-    if Node.state.is_terminal():
-        return Node.state.evaluate()
+        v = -1
+        for n in node.children:
+            v = max(v,min_value(n))
+        return v
 
-    v = 100
-    for n in Node.children:
-        v = min(v,max_value(n))
-    return v
+    def min_value(node = None):
+        if node.state.is_terminal():
+            return node.state.evaluate()
 
-
-'''
-def minimax(state, depth, maximizing):
-    """
-    Implémentation de l'algorithme Minimax.
-
-    :param etat: L'état actuel du jeu.
-    :param profondeur: La profondeur de recherche maximale.
-    :param maximisant: Booléen indiquant si c'est le tour du joueur MAX.
-    :return: La valeur minimax de l'état actuel.
-    """
-
-    # Si l'état est terminal ou la profondeur est atteinte, on évalue l'état
-    if depth == 0 or state.is_terminal():
-        return state.evaluate()
-
-     # Cas du joueur MAX (veut maximiser son score)
-    if maximizing:
-        max_value = float('-inf')
-        for successor in state.generate_successors():
-            value = minimax(successor, depth - 1, False)
-            max_value = max(max_value, value)
-        return max_value
-
-    # Cas du joueur MIN (veut minimiser le score de MAX)
-    else:
-        min_value = float('inf')
-        for successor in state.generate_successors():
-            value = minimax(successor, depth - 1, True)
-            min_value = min(min_value, value)
-        return min_value
-'''
+        v = 100
+        for n in node.children:
+            v = min(v,max_value(n))
+        return v
